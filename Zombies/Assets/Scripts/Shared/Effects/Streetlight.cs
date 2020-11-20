5214022a8e4bba9e9f286bde67ce07a01ae35677
Joan Sala Soler <contacte@joansala.com>
2020-11-20 22:19:43 +0100

    Adds streetlights on the main road
    Former-commit-id: c97db1779ab24d9c91ea1932dd6e1c9428c0ee3a

---
+++ Zombies/Assets/Scripts/Shared/Effects/Streetlight.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Game.Shared {
+
+    /**
+     *
+     */
+    public class Streetlight : MonoBehaviour {
+
+        /** Delay after wich the light will turn on */
+        public float delay = 510.0f;
+
+        /** Material for when the light is off */
+        public Material offMaterial = null;
+
+        /** Material for when the light is on */
+        public Material onMaterial = null;
+
+        /** Mesh renderer of the object */
+        private MeshRenderer meshRenderer = null;
+
+        /** Spot light of the object */
+        private Light spotLight = null;
+
+
+        /**
+         * Initialization.
+         */
+        private void OnEnable() {
+            meshRenderer = GetComponentInChildren<MeshRenderer>();
+            spotLight = GetComponentInChildren<Light>();
+            StartCoroutine(TurnOnLight());
+        }
+
+
+        /**
+         *
+         */
+        private IEnumerator TurnOnLight() {
+            yield return new WaitForSeconds(delay);
+
+            spotLight.enabled = true;
+            meshRenderer.material = onMaterial;
+            yield return new WaitForSeconds(Random.Range(0.1f, 0.3f));
+
+            spotLight.enabled = false;
+            meshRenderer.material = offMaterial;
+            yield return new WaitForSeconds(Random.Range(0.1f, 0.3f));
+
+            spotLight.enabled = true;
+            meshRenderer.material = onMaterial;
+        }
+    }
+}
